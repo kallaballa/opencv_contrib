ad9f748aa8bb612754b9dc379f862922b39a0249
kallaballa <amir@viel-zu.com>
2025-01-09 01:46:05 +0100

    made opencl optional

--- modules/v4d/include/opencv2/v4d/detail/cl.hpp
+++ modules/v4d/include/opencv2/v4d/detail/cl.hpp
@@ -1,6 +1,8 @@
 #ifndef MODULES_V4D_INCLUDE_OPENCV2_V4D_DETAIL_CL_HPP_
 #define MODULES_V4D_INCLUDE_OPENCV2_V4D_DETAIL_CL_HPP_
 
+#ifdef HAVE_OPENCL
+
 #ifndef CL_TARGET_OPENCL_VERSION
 #  define CL_TARGET_OPENCL_VERSION 120
 #endif
@@ -8,6 +10,8 @@
 #  include <OpenCL/cl_gl_ext.h>
 #else
 #  include <CL/cl_gl.h>
+#endif
+
 #endif
 
 #endif /* MODULES_V4D_INCLUDE_OPENCV2_V4D_DETAIL_CL_HPP_ */
